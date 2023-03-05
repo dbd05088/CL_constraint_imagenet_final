27f79aa7fd0c05adf28773dfcf3ffe66180b4910
smh <dbd05088@naver.com>
2023-03-05 19:51:33 +0900

    ex

--- ex.sh
+++ ex.sh
@@ -1,25 +1,25 @@
 #/bin/bash
 
 # CIL CONFIG
-NOTE="cifar10_sigma10_test" # Short description of the experiment. (WARNING: logs/results with the same note will be overwritten!)
+NOTE="ours_sigma10_imagenet_k_4_t_0.125_real_iter_0.25" # Short description of the experiment. (WARNING: logs/results with the same note will be overwritten!)
 MODE="ours"
 
-K_COEFF="2"
-TEMPERATURE="2"
+K_COEFF="4"
+TEMPERATURE="0.125"
 
 TRANSFORM_ON_GPU="--transform_on_gpu"
-N_WORKER=2
-FUTURE_STEPS=2
+N_WORKER=5
+FUTURE_STEPS=3
 EVAL_N_WORKER=4
 EVAL_BATCH_SIZE=1000
-USE_KORNIA="--use_kornia"
-#USE_KORNIA=""
+#USE_KORNIA="--use_kornia"
+USE_KORNIA=""
 UNFREEZE_RATE=0.25
 SEEDS="1"
 
 
-DATASET="cifar10" # cifar10, cifar100, tinyimagenet, imagenet
-ONLINE_ITER=0.6
+DATASET="imagenet" # cifar10, cifar100, tinyimagenet, imagenet
+ONLINE_ITER=0.3
 
 SIGMA=10
 REPEAT=1
@@ -51,7 +51,7 @@ elif [ "$DATASET" == "imagenet" ]; then
     MEM_SIZE=1281167
     N_SMP_CLS="3" K="3"
     CANDIDATE_SIZE=1000
-    MODEL_NAME="resnet18" EVAL_PERIOD=2000
+    MODEL_NAME="resnet18" EVAL_PERIOD=8000
     BATCHSIZE=256; LR=3e-4 OPT_NAME="adam" SCHED_NAME="default" IMP_UPDATE_PERIOD=10
 
 else
@@ -61,12 +61,12 @@ fi
 
 for RND_SEED in $SEEDS
 do
-    CUDA_VISIBLE_DEVICES=2 python main_new.py --mode $MODE --transform_on_worker \
+    CUDA_VISIBLE_DEVICES=0 nohup python main_new.py --mode $MODE \
     --dataset $DATASET --unfreeze_rate $UNFREEZE_RATE $USE_KORNIA --k_coeff $K_COEFF --temperature $TEMPERATURE \
     --sigma $SIGMA --repeat $REPEAT --init_cls $INIT_CLS \
-    --rnd_seed $RND_SEED \
+    --rnd_seed $RND_SEED --data_dir ../../../../Downloads/ILSVRC/Data/CLS-LOC/ \
     --model_name $MODEL_NAME --opt_name $OPT_NAME --sched_name $SCHED_NAME \
     --lr $LR --batchsize $BATCHSIZE \
     --memory_size $MEM_SIZE $TRANSFORM_ON_GPU --online_iter $ONLINE_ITER \
-    --note $NOTE --eval_period $EVAL_PERIOD --imp_update_period $IMP_UPDATE_PERIOD $USE_AMP --n_worker $N_WORKER --future_steps $FUTURE_STEPS --eval_n_worker $EVAL_N_WORKER --eval_batch_size $EVAL_BATCH_SIZE
+    --note $NOTE --eval_period $EVAL_PERIOD --imp_update_period $IMP_UPDATE_PERIOD $USE_AMP --n_worker $N_WORKER --future_steps $FUTURE_STEPS --eval_n_worker $EVAL_N_WORKER --eval_batch_size $EVAL_BATCH_SIZE &
 done
